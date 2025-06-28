bf123656314af75a8865026c7932d4f3aff03bad
supersunho <supersunho@gmail.com>
2025-06-28 12:35:47 +0900

    update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -68,7 +68,7 @@ validate_environment() {
 # Permission setup with optimization
 setup_permissions() {
     print_info "Setting up file permissions..."
-    
+    chown -R "steam:steam" "/home/steam"
     local directories=(
         "/home/steam/palworld_server"
         "/home/steam/backups"
