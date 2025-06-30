d3609e93db1adee13c6e5ee3b23d1214634a4c75
supersunho <supersunho@gmail.com>
2025-06-30 11:48:35 +0900

    update src/backup/__init__.py

--- src/backup/__init__.py
+++ src/backup/__init__.py
@@ -3,6 +3,6 @@ Backup management package
 Automated backup scheduling and management for Palworld server
 """
 
-from .backup_manager import get_backup_manager, BackupManager
+from .backup_manager import get_backup_manager, EnhancedBackupManager
 
-__all__ = ['get_backup_manager', 'BackupManager']
+__all__ = ['get_backup_manager', 'EnhancedBackupManager']
